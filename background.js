54df9d26778a188cab43ccfa5dd331d72057489b
Ahmet Atasoy <menthena@gmail.com>
2020-05-08 14:57:18 +0100

    Update background

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
-chrome.runtime.onInstalled.addListener(() => {
+const refresh = () => {
   const fiveMinutes = 5 * 60 * 1000;
   setInterval(() => {
     fetch("https://discovery.okta.com", { mode: "no-cors" });
   }, fiveMinutes);
-});
+};
+
+chrome.runtime.onStartup.addListener(refresh);
+chrome.runtime.onInstalled.addListener(refresh);
